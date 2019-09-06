1ceee288526e4fbbcd12b037a1f6c6182d03191a
yiqing <yiqing_95@qq.com>
2019-09-07 01:25:19 +0800

    控制台 支持kingpin

--- bundles/hello/delivery/console/bundle.go
+++ bundles/hello/delivery/console/bundle.go
@@ -1,10 +1,11 @@
 package console
 
 import (
-	"fmt"
 	"github.com/fspace/ecm/bundles/hello"
+	"github.com/fspace/ecm/bundles/hello/delivery/console/cmds"
 	"github.com/fspace/ecm/core/app/console"
 	"github.com/spf13/viper"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"log"
 )
 
@@ -27,21 +28,32 @@ type Bundle struct {
 // TODO 校验配置是否正确！
 func (b *Bundle) Configure(viper *viper.Viper) error {
 	log.Println("hello-bundle::configure")
-	log.Println(viper.AllKeys())
-	v := viper.Sub("hello-bundle")
-
-	confVal := v.Get("someKey")
-	fmt.Println("someKey is : ", confVal)
-	//fmt.Println(viper.AllKeys())
-	conf := hello.Config{}
-	v.Unmarshal(&conf)
-	fmt.Printf("config is : %#v \n", conf)
-	b.Config = &conf
+	//log.Println(viper.AllKeys())
+	//v := viper.Sub("hello-bundle")
+	//
+	//confVal := v.Get("someKey")
+	//fmt.Println("someKey is : ", confVal)
+	////fmt.Println(viper.AllKeys())
+	//conf := hello.Config{}
+	//v.Unmarshal(&conf)
+	//fmt.Printf("config is : %#v \n", conf)
+	//b.Config = &conf
 
 	return nil
 }
 
 func (b *Bundle) Init() error {
 	log.Println("hello-bundle::init")
+
+	initCommands(b.App.KingpinApp)
+
 	return nil
+}
+
+func initCommands(app *kingpin.Application) {
+	cmd_addUser := &cmds.AddUserCommand{}
+	addUser := app.Command("user-add", "create users.").Action(cmd_addUser.Run)
+	// 配置命令入参flag
+	addUser.Flag("data", "user data.").Short('d').StringVar(&cmd_addUser.Data)
+	_ = addUser
 }
